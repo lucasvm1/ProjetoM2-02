49416d4e571f7a25f863b0305e51529a4ff2a4a2
LucasVM1 <lucas.vianam@outlook.com>
2022-03-12 17:10:33 -0300

    main

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () =>
+  console.log("Servidor ordnado em http://localhost:3000")
+);
