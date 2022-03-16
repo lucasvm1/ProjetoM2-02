3fb601c1a78ae107101e4527be34e9cb50968f27
LucasVM1 <lucas.vianam@outlook.com>
2022-03-15 21:50:09 -0300

    main

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const express = require('express');
+const express = require("express");
+const path = require("path")
 const app = express();
 
-const port = 3000
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")))
+
+const pokedex = [
+  {
+    id: 1,
+    nome: 'Bulbasaur',
+    descricao: 'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+    tipo: 'Grass',
+    imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+  },
+  {
+    id: 4,
+    nome: 'Charmander',
+    descricao: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+    tipo: 'Fire',
+    imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
+  },
+  {
+    id: 7,
+    nome: 'Squirtle',
+    descricao: 'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
+    tipo: 'Water',
+    imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+  }
+
+]
+
+
+// ROTAS
+
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {pokedex});
 });
 
 app.listen(port, () =>
-  console.log('Servidor rodando em http://localhost:3000')
-)
+  console.log("Servidor rodando em http://localhost:3000")
+);
