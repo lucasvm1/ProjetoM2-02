5de8be5e42d49eda3e076a0b775592d190524473
LucasVM1 <lucas.vianam@outlook.com>
2022-03-17 20:53:49 -0300

    main

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const path = require("path")
+import express from "express";
+import path from 'path'
 const app = express();
+
+const __dirname = path.resolve(path.dirname(''))
+
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, "public")))
