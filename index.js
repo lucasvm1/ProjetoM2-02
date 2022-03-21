256470ba4833215459480a1c7fd79d8786ba2b43
LucasVM1 <lucas.vianam@outlook.com>
2022-03-20 23:00:05 -0300

    main

--- index.js
+++ index.js
@@ -4098,7 +4098,7 @@ const pokedexObj = {
 
 const pokedex = pokedexObj.pokemon
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
